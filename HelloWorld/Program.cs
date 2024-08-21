250eb9ba7d3f5edd1d4d385117f2327f27886d0d
Klejker <128978446+Klejker@users.noreply.github.com>
2024-08-21 15:00:04 +0200

    Lektion 08/21 tränat på HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Nu mitt första c# pogram");
+Console.WriteLine("Jag är en konsol applikation");
+Console.Write("Vad heter du? ");
+string namn = Console.ReadLine() ?? "";
+Console.WriteLine("Hej " + namn);
+Console.Write("Vad gör du på fritiden? ");
+string fritid = Console.ReadLine() ?? "";
+Console.WriteLine("Jag med, " + fritid + " är kul");
+
+Console.ResetColor();
+
+Console.WriteLine("Nu har jag ändrat färg på bakgrund och text");
+Console.WriteLine("Vad var det bästa med OS 2024 i Paris, då de spelade under Efieltornet? ");
+string os = Console.ReadLine() ?? "";
+Console.WriteLine("Ja, " + os + " var det bästa med OS 2024");
