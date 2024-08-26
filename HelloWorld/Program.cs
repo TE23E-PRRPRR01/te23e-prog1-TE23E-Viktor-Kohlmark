91b99a193beb137c4dcee260152ec98075bba76d
Klejker <128978446+Klejker@users.noreply.github.com>
2024-08-26 13:47:32 +0200

    Test 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,19 +1,18 @@
 ﻿Console.Clear();
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Skibidi, världen!");
 
-Console.WriteLine("Nu mitt första c# pogram");
-Console.WriteLine("Jag är en konsol applikation");
+Console.WriteLine("Nu mitt första c# program");
+Console.WriteLine("Jag är en rizlerapplikation");
 Console.Write("Vad heter du? ");
 string namn = Console.ReadLine() ?? "";
-Console.WriteLine("Hej " + namn);
+Console.WriteLine("Skibidi " + namn);
 Console.Write("Vad gör du på fritiden? ");
 string fritid = Console.ReadLine() ?? "";
-Console.WriteLine("Jag med, " + fritid + " är kul");
+Console.WriteLine("Skibidi också, " + fritid + " är kul");
 
 Console.ResetColor();
 
 Console.WriteLine("Nu har jag ändrat färg på bakgrund och text");
-Console.WriteLine("Vad var det bästa med OS 2024 i Paris, då de spelade under Efieltornet? ");
+Console.WriteLine("Vad var det bästa med OS 2024 i Paris, då de spelade under Eiffeltornet? ");
 string os = Console.ReadLine() ?? "";
-Console.WriteLine("Ja, " + os + " var det bästa med OS 2024");
+Console.WriteLine("Skibidi, " + os + " var det bästa med OS 2024");
