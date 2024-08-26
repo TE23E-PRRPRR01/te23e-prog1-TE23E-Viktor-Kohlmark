573a83e415d2f3ed6afda47936c2dc47be34c99e
Klejker <128978446+Klejker@users.noreply.github.com>
2024-08-26 14:31:25 +0200

    Skapat ett pogram för att registera förnamn och efternamn via Console.WriteLine

---
+++ Kapitel-1/Registering/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+Console.WriteLine("Program för att registera förnamn och efternamn");
+
+Console.Write("Ange förnamn: ");
+string förnamn = Console.ReadLine();
+Console.Write("Ange efternamn: ");
+string efternamn = Console.ReadLine();
+
+Console.WriteLine($"Du har angett förnamn: {förnamn} och efternamn: {efternamn}");
+
