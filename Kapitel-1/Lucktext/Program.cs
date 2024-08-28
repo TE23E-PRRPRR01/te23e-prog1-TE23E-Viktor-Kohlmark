b86544a9273c3f00e36a2a11ae858abf0986af3f
Klejker <128978446+Klejker@users.noreply.github.com>
2024-08-28 14:22:20 +0200

    Program to put input form the user in to a sentence

---
+++ Kapitel-1/Lucktext/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello");
+
+Console.Write("Name : ");
+String name = Console.ReadLine();
+
+Console.Write("Sport : ");
+String game = Console.ReadLine();
+
+Console.WriteLine("Levndas ort : ");
+String city = Console.ReadLine();
+
+Console.Clear();
+
+if(city == "Tullinge")
+{
+	Console.WriteLine("Good");
+}
+else
+{
+    Console.WriteLine("Bad");
+}
+
+Console.WriteLine($"Hello {name}! Welcome to the game {game} in the city of {city}.");
