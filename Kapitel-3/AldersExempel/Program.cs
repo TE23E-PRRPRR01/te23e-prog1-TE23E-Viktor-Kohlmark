cd784b8c916becb4c949a5ae061fb7826641ccc0
Klejker <128978446+Klejker@users.noreply.github.com>
2024-09-09 14:35:07 +0200

    Tränat på listor, if statment, större eller minder och olika typer av loops.

---
+++ Kapitel-3/AldersExempel/Program.cs
@@ -0,0 +1,25 @@
+﻿List<(string app, int age)> 
+apps = new List<(string, int)>
+{
+    ("Roblox", 10),
+    ("Tiktok", 20),
+    ("Facebook", 30),
+    ("MinaSidor", 40),
+    ("Discord", 50),
+    ("Mincraft", 60),
+    ("Spotify", 70),
+    ("Netflix", 80),
+    ("Youtube", 90),
+    ("Google", 100)
+};
+
+Console.Clear();
+Console.Write("Kan du skriva din ålder?");
+
+int age = int.Parse(Console.ReadLine());
+
+foreach (var app in apps) {
+    if (age >= app.age) {
+        Console.WriteLine($"Du är {age} år gammal och kan använda {app.app}");
+    }
+}
---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Hej, idag ska du köra ett spel.");
+Console.WriteLine("Spelet går ut på att du ska gissa ett hemligt number");
+Thread.Sleep(5000);
+
+Console.Clear();
+Console.WriteLine("Gissa numret mellan 1 och 100");
+
+Random random = new Random();
+int baseNumber = random.Next(1, 101); // Det hemliga numret
+
+int number = 0 ; 
+
+
+while (number != baseNumber){
+
+string textNumber = Console.ReadLine(); // Läs in texten 
+number = int.Parse(textNumber); // Konvertera texten till ett heltal
+
+    if (number > baseNumber)
+    {
+        Console.WriteLine("För högt!");  
+    }
+    else if (number < baseNumber)
+    {
+        Console.WriteLine("För lågt!");
+    } 
+    else 
+    {
+        Console.WriteLine($"Rätt svar! {number} är korrekt!");
+    }
+}
+
+
+
+
+
+
