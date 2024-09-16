e8c43f738277e3e9abccc6d62ab59d94c6fa04fe
Klejker <128978446+Klejker@users.noreply.github.com>
2024-09-16 14:38:00 +0200

    Tränat på while loopar och hur "++" fungerar samt break

---
+++ Kapitel-4/Gissatalet2/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Hej, idag ska du köra ett spel.");
+Console.WriteLine("Spelet går ut på att du ska gissa ett hemligt number");
+
+Thread.Sleep(3000);
+Console.Clear();   
+
+Console.WriteLine("Vilka numer vill du gissa mellan?");
+
+Console.WriteLine("Skriv in det lägsta numret");
+int min = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Skriv in det högsta numret");
+int max = int.Parse(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Gissa numret mellan {min} och {max}");
+
+Random random = new Random();
+int baseNumber = random.Next(min, max + 1); // Det hemliga numret
+
+int number = 0;
+
+int antalgissningar = 0;
+
+while (number != baseNumber)
+{
+
+    string textNumber = Console.ReadLine(); // Läs in texten 
+    number = int.Parse(textNumber); // Konvertera texten till ett heltal
+
+    Console.WriteLine("Vill du avsluta splet? (ja/nej)");
+    string jaNej = Console.ReadLine();
+
+    if (jaNej == "ja")
+    {
+        Console.WriteLine("Spelet avslutas");
+        break;
+    }else if (jaNej == "nej")
+    {
+        Console.WriteLine("Spelet fortsätter");
+    }
+
+    antalgissningar++;
+
+    if (number > baseNumber)
+    {
+        Console.WriteLine("För högt!");
+    }
+    else if (number < baseNumber)
+    {
+        Console.WriteLine("För lågt!");
+    }
+    else
+    {
+        Console.WriteLine($"Rätt svar! {number} är korrekt!");
+        Console.WriteLine("Antal gissningar: " + antalgissningar);
+    }
+}
+
+
