a8bd4df6c281d8df3eae7a9b19047fc4a631a4b3
kohlmark.viktor <viktor.kohlmark.ntig.se>
2024-11-06 14:56:13 +0100

    Repeterat loopar och slumptal

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+Console.WriteLine("==================================");
+
+// Globala variabler 
+// En lista för heltal 
+List<int> listaslumptal = [];
+
+
+Console.WriteLine("Skriv vilka tal som ska slumpas");
+Console.WriteLine("==================================");
+
+// Fråga användaren om min och max värden samt hur många gåner det ska slumpas
+Console.Write("Lägsta tal: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Högsta tal: ");
+int max = int.Parse(Console.ReadLine());
+Console.Write("Hur många gånger ska det slumpas: ");
+int antal = int.Parse(Console.ReadLine());
+
+// Loopa 5 ggr 
+for (int i = 0; i < antal; i++)
+{
+
+    int slumptal = 0;
+    slumptal = Random.Shared.Next(min, max + 1);
+
+    // Lägg till slumptalet i listan
+    listaslumptal.Add(slumptal);
+    Console.WriteLine($"Slumptal {i + 1}: {slumptal}");
+
+}
