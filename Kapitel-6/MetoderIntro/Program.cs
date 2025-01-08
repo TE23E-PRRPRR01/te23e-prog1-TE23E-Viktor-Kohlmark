4a4f87632ee77e09dc14a09a90c8afc453de15ba
kohlmark.viktor <viktor.kohlmark.ntig.se>
2025-01-08 15:03:23 +0100

    Jobbat på metoder

---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+// Anropa metoden 
+
+sägHej();
+
+// Anropa metoden meny 
+VisaMeny();
+
+/// <summary>
+/// Metod som skriver ut en hälsning 
+/// </summary>
+
+static void sägHej()
+{
+    Console.WriteLine("Hej ");
+}
+
+/// <summary>
+/// Metod för att visa en meny 
+/// </summary>
+
+static void VisaMeny()
+{
+    Console.WriteLine(""" 
+    1. Lägg till uppgift
+    2. Ta bort uppgift 
+    3. Skriv ut alla uppgifter 
+    4. Avsluta
+    """);
+}
+
---
+++ Kapitel-6/MetoderMassor/Program.cs
@@ -0,0 +1,25 @@
+﻿/* *********************************************
+***                M A I N                   ***
+********************************************* */
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejNamn("Johnny");
+KvadreraTal(10); 
+
+/* *********************************************
+***               M E T O D E R              ***
+********************************************* */
+/// <summary>
+/// 
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
+
+static void KvadreraTal(int tal) {
+    Console.WriteLine($"{{tal} i kvadrat är {tal*tal}}");
+}
